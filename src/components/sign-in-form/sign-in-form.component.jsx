5f79a330ca032c3bdcddc4ce4d60f5591bd9b8b8
Michael Duren <michaeld@michaelduren.com>
2022-09-29 20:53:22 -0500

    created auth page component alongside sign in form and styling

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,104 @@
+import {
+  signInWithGooglePopup,
+  signInWithFacebookPopup,
+  createUserDocumentFromAuth,
+  signInAuthWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils';
+
+import './sign-in-form.styles.scss';
+
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+
+import { useState } from 'react';
+
+const defaultFormFields = {
+  email: '',
+  password: '',
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  const logFacebookUser = async () => {
+    const { user } = await signInWithFacebookPopup();
+    await createUserDocumentFromAuth(user);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { user } = await signInAuthWithEmailAndPassword(email, password);
+      console.log(user);
+      resetFormFields();
+    } catch (err) {
+      switch (err.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('user not found, please sign up for an account');
+          break;
+        default:
+          console.log(err);
+      }
+    }
+  };
+
+  return (
+    <div className="sign-in-container">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        ></FormInput>
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+      </form>
+      <div className="buttons-container">
+        <Button type="submit" onClick={handleSubmit}>
+          Sign In
+        </Button>
+        <br />
+        <Button type="button" buttonType="google" onClick={logGoogleUser}>
+          Sign in with Google
+        </Button>
+        <br />
+        <Button type="button" buttonType="facebook" onClick={logFacebookUser}>
+          Sign in with Facebook
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SignInForm;
