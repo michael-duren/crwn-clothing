c6b8219154798ca3c89d4b769511b3ce9dba0c7f
Michael Duren <michaeld@michaelduren.com>
2022-10-08 15:55:43 -0500

    CONVERTED cart-item and directory.styles to styled-component

--- src/components/categories/directory.component.jsx
+++ src/components/categories/directory.component.jsx
@@ -1,6 +1,6 @@
 import DirectoryItem from '../directory-item/directory-item.component';
 
-import './directory.styles.scss';
+import { CategoriesContainer } from './directory.styles';
 
 const categories = [
   {
@@ -37,11 +37,11 @@ const categories = [
 
 const Directory = () => {
   return (
-    <div className="categories-container">
+    <CategoriesContainer>
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category} />
       ))}
-    </div>
+    </CategoriesContainer>
   );
 };
 
