10b7838d7e75df2d7d7d23ea4abef12d840714a1
Michael Duren <michaeld@michaelduren.com>
2022-10-16 14:18:39 -0500

    Cleaned up code, setup redux, convert user context

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ import Authentication from './routes/authentication/authentication.componet';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { setCurrentUser } from './store/user/user.action';
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from './utils/firebase/firebase.utils';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
