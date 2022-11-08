9f3de279736d7ded943dfda2eed54025b0e663d6
Michael Duren <michaeld@michaelduren.com>
2022-11-08 17:16:12 -0600

    trying to setup redux

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
@@ -5,8 +8,26 @@ import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.componet';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import { setCurrentUser } from './store/user/user.action';
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from './utils/firebase/firebase.utils';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
