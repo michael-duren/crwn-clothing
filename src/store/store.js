dc15689fb4510f0d2f000755173a2b716718bbb6
Michael Duren <michaeld@michaelduren.com>
2022-11-08 17:36:59 -0600

    user Redux setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,4 @@
-import { compose, applyMiddleware } from 'redux';
-import { legacy_createStore as createStore } from 'redux';
-
+import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
