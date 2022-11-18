40b73deefed7f12a1f5ed2e0e104d6adb46665a0
Michael Duren <michaeld@michaelduren.com>
2022-11-17 20:01:34 -0600

    migrated categories to redux-saga

---
+++ src/store/categories/category.saga.js
@@ -0,0 +1,33 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+import {
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from './category.action';
+
+import { CATEGORIES_ACTION_TYPES } from './category.types';
+
+export function* fetchCategoriesAsync() {
+  try {
+    const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
+    yield put(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    yield put(fetchCategoriesFailed(error));
+  }
+}
+
+export function* onFetchCategories() {
+  yield takeLatest(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
+    fetchCategoriesAsync
+  );
+}
+
+export function* categoriesSaga() {
+  // run everything inside and only complete when everything is done
+  // all says you can give me an array of different things you are calling
+  // inside the array is where we add the genrators after we write them
+  yield all([call(onFetchCategories)]);
+}
