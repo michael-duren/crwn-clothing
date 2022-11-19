3fa30d4666c064e9a33b3ae568f6968a52d66e62
Michael Duren <michaeld@michaelduren.com>
2022-11-19 13:29:19 -0600

    finished migrating users to saga

--- src/store/user/user.saga.js
+++ src/store/user/user.saga.js
@@ -7,6 +7,8 @@ import {
   signInFailed,
   signUpSuccess,
   signUpFailed,
+  signOutSuccess,
+  signOutFailed,
 } from './user.action';
 
 import {
@@ -16,6 +18,7 @@ import {
   signInWithFacebookPopup,
   signInAuthWithEmailAndPassword,
   createAuthUserWithEmailAndPassword,
+  signOutUser,
 } from '../../utils/firebase/firebase.utils';
 
 export function* getSnapShotFromUserAuth(userAuth, additionalDetails) {
@@ -85,6 +88,15 @@ export function* signUp({ payload: { email, password, displayName } }) {
   }
 }
 
+export function* signOut() {
+  try {
+    yield call(signOutUser);
+    yield put(signOutSuccess());
+  } catch (error) {
+    yield put(signOutFailed(error));
+  }
+}
+
 export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
   yield call(getSnapShotFromUserAuth, user, additionalDetails);
 }
@@ -116,6 +128,10 @@ export function* onSignUpSuccess() {
   yield takeLatest(USER_ACTION_TYPES.SIGN_UP_SUCCESS, signInAfterSignUp);
 }
 
+export function* onSignOutStart() {
+  yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
   yield all([
     call(onCheckUserSession),
@@ -124,5 +140,6 @@ export function* userSagas() {
     call(onEmailSignInStart),
     call(onSignUpStart),
     call(onSignUpSuccess),
+    call(onSignOutStart),
   ]);
 }
